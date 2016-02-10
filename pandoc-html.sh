219401805b516c66889dab01576bc0da23b0b81a
Kageetai <ma@kageetai.net>
2016-02-10 22:01:16 +0100

    updated html script with template

--- pandoc-html.sh
+++ pandoc-html.sh
@@ -1,3 +1,8 @@
 # Generate HTML for preview
 	pandoc bibliography.yml metadata.yml markdown/*.md -o dist/index.html \
-	-s
+	-s \
+	--template templates/template.html \
+	-c style.css
+	cp templates/style.css dist/style.css
+	mkdir -p dist/images/
+	cp images/* dist/images/
