68ace3b841e6fa605f7be02df6a6f3372fa835d1
Kageetai <ma@kageetai.net>
2016-02-12 16:14:28 +0100

    added IEEE.csl style for citations

--- pandoc-pdf.sh
+++ pandoc-pdf.sh
@@ -2,5 +2,6 @@
 pandoc bibliography.yml metadata.yml content/*.md -o dist/document.pdf \
 	--latex-engine=xelatex \
 	--filter pandoc-citeproc \
+	--csl=templates/ieee.csl \
 	--number-sections --reference-links
 rm -rf tex2pdf.*
