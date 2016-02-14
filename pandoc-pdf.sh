03781e235ec82813d83adfd0aefc74cafe91a843
Kageetai <ma@kageetai.net>
2016-02-14 13:19:24 +0100

    new template for title page

--- pandoc-pdf.sh
+++ pandoc-pdf.sh
@@ -1,6 +1,7 @@
 # Generate PDF for thesis
 pandoc bibliography.yml metadata.yml content/*.md -o dist/document.pdf \
 	--latex-engine=xelatex \
+	--template=templates/template.tex \
 	--filter pandoc-citeproc \
 	--csl=templates/ieee.csl \
 	--number-sections --reference-links
